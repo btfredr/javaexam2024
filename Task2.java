183aebf756fa5b5250443b9664fb53b1dcbb4977
Fredrik Fordelsen <fredrikf2000@gmail.com>
2025-04-23 13:06:52 +0200

    asking user for input and saving as variable

--- Task2.java
+++ Task2.java
@@ -12,7 +12,14 @@
     number of the row.
  */
 
+import java.util.Scanner;
 
 public class Task2 {
-    
+    public static void main(String [] args ) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Please enter height of the pyramid: ");
+        int height = scanner.nextInt();
+
+        
+    }
 }
